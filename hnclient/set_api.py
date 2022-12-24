e72a27d77026b77b1efc736889b3f2c1bb01292a
InquestGeronimio <rcosta84@gmail.com>
2022-12-23 23:46:54 -0500

    edit main.py :rocket:

--- hnclient/set_api.py
+++ hnclient/set_api.py
@@ -3,6 +3,6 @@ from hnclient.client import HTTPClient
 
 class HTTPHandler(Manager):
 
-        def call_api(self, func: object, cache: bool):
+        def call_api(self, func: object, cache: bool) -> HTTPClient:
 
             return HTTPClient(func, cache)()
