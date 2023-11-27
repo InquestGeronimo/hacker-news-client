317597edcbc61d16fef147e57c190813dc540ace
InquestGeronimo <rcosta84@gmail.com>
2023-11-27 11:34:29 -0500

    added docstrings

--- src/hnclient/client.py
+++ src/hnclient/client.py
@@ -3,11 +3,29 @@ from typing import List, Dict
 from .set_api import HTTPHandler
 
 class HackerNewsClient(HTTPHandler):
+    """
+    A client for interacting with the Hacker News API.
+
+    :param disable_cache: Set to True to disable caching.
+    """
+
     def __init__(self, disable_cache: bool = False):
+        """
+        Initialize a new HackerNewsClient.
+
+        :param disable_cache: Set to True to disable caching.
+        """
         super().__init__()
         self.disable_cache = disable_cache
 
     def get_stories(self, story: str = "top", descending: bool = False) -> List[Dict]:
+        """
+        Get a list of Hacker News stories.
+
+        :param story: The type of story to retrieve (e.g., "top").
+        :param descending: Sort stories by score in descending order if True.
+        :return: List of story dictionaries.
+        """
         payload = []
         story_ids = self.call_api(self.get_story_ids(story), self.disable_cache)
 
@@ -22,8 +40,22 @@ class HackerNewsClient(HTTPHandler):
 
     @staticmethod
     def get_item(item: str, data: List[Dict]) -> List:
+        """
+        Extract a specific item from a list of dictionaries.
+
+        :param item: Key to extract from each dictionary.
+        :param data: List of dictionaries.
+        :return: List of extracted items.
+        """
         return [obj[item] for obj in data]
 
     @staticmethod
     def get_random_story(data: List[Dict]) -> Dict:
+        """
+        Get a random story from a list of stories.
+
+        :param data: List of story dictionaries.
+        :return: Random story dictionary.
+        """
         return random.choice(data)
+
--- src/hnclient/set_api.py
+++ src/hnclient/set_api.py
@@ -2,7 +2,24 @@ from .utils import DataProcessor
 from .session import FirebaseHTTPClient
 
 class HTTPHandler(DataProcessor):
+    """
+    Handler for making API calls using the HTTPClient.
 
-        def call_api(self, func: object, cache: bool) -> FirebaseHTTPClient:
+    This class inherits from DataProcessor and provides 
+    a method for making API calls using an instance of 
+    FirebaseHTTPClient.
 
-            return FirebaseHTTPClient(func, cache)()
+    :param func: The API function to call.
+    :param cache: Set to True to enable caching.
+    :return: An instance of FirebaseHTTPClient.
+    """
+
+    def call_api(self, func: object, disable_cache: bool) -> FirebaseHTTPClient:
+        """
+        Make an API call using FirebaseHTTPClient.
+
+        :param func: The API function to call.
+        :param cache: Set to True to disagble caching.
+        :return: An instance of FirebaseHTTPClient.
+        """
+        return FirebaseHTTPClient(func, disable_cache)()
